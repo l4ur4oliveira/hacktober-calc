8425dfc984c71bf70c488beeab8437043a2ba933
Laura de Oliveira <laura@pmweb.com.br>
2021-09-09 00:30:39 -0300

    Add a toolbar and theme options

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
 // The calculator magic works here
+
+const root = document.querySelector('html');
+
+const toolbarThemes = document.querySelector('.toolbar-option__themes');
+const themesOptions = document.querySelector('.toolbar__themes');
+toolbarThemes.addEventListener('click', event => {
+  event.preventDefault();
+  
+  themesOptions.classList.toggle('open');
+});
+
+const themeItems = document.querySelectorAll('.toolbar__themes li');
+themeItems.forEach(theme => {
+  theme.addEventListener('click', item => {
+    root.classList.replace(root.className, item.target.className);
+  })
+});
