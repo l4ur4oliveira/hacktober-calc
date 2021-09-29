843d862f6cb3b0e3375b4ed75e5e8a8d47b38f12
Laura de Oliveira <laura@pmweb.com.br>
2021-09-28 22:14:16 -0300

    Add input and clear screen actions

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,44 @@
-// The calculator magic works here
-
 const root = document.querySelector('html');
 
-const toolbarThemes = document.querySelector('.toolbar-option__themes');
-const themesOptions = document.querySelector('.toolbar__themes');
+const toolbarThemes = document.querySelector('.toolbar-option.themes');
+const toolbarClear = document.querySelector('.toolbar-option.clear');
+const themeList = document.querySelector('.toolbar__themes');
+
 toolbarThemes.addEventListener('click', event => {
   event.preventDefault();
   
-  themesOptions.classList.toggle('open');
+  themeList.classList.toggle('open');
 });
 
-const themeItems = document.querySelectorAll('.toolbar__themes li');
-themeItems.forEach(theme => {
+toolbarClear.addEventListener('click', event => {
+  event.preventDefault();
+  
+  const screen = document.querySelectorAll('#screen span');
+  screen.forEach(item => {
+    if (item.innerHTML != '') {
+      item.innerHTML = '';
+    }
+  });
+});
+
+const themeListItems = document.querySelectorAll('.toolbar__themes li');
+themeListItems.forEach(theme => {
   theme.addEventListener('click', item => {
     root.classList.replace(root.className, item.target.className);
+    themeList.classList.remove('open');
   })
+});
+
+// Handle buttons
+function showCurrentNumber(number) {
+  const currentNumber = document.querySelector('#screen .screen__current');
+  currentNumber.innerHTML += number;
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+numberButtons.forEach(number => {
+  number.addEventListener('click', () => {
+    showCurrentNumber(number.textContent);
+  });
 });
