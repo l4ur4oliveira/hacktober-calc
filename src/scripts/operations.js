471583e823ea8211d0aae6a3c89f50f92e9ed7fe
monieshravichandrran <monieshravichandrran@gmail.com>
2022-10-16 01:24:07 +0530

    added scientific log power and factorial

--- src/scripts/operations.js
+++ src/scripts/operations.js
@@ -12,4 +12,22 @@ function division(x, y) {
   return x / y;
 }
 
-export { sum, subtraction, multiplication, division };
+function log(x){
+  return Math.log10(x);
+}
+
+function power(x,y){
+  return Math.pow(x,y);
+}
+
+function factorial(x){
+  let power = 1;
+  for(let i=2;i<=x;i++){
+    power = power*i;
+  }
+  return power;
+}
+
+
+
+export { sum, subtraction, multiplication, division, log, power, factorial  };
