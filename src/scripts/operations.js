2dfe1b9f0ac2feae2463f7421c03ed10b21e2f02
Laura de Oliveira <l4ur4.88@gmail.com>
2024-04-15 17:51:34 -0300

    refactor: Improve factorial operation

--- src/scripts/operations.js
+++ src/scripts/operations.js
@@ -1,33 +1,36 @@
 // The calculator magic works here
 function sum(x, y) {
-  return x + y;
+    return x + y;
 }
 function subtraction(x, y) {
-  return x - y;
+    return x - y;
 }
 function multiplication(x, y) {
-  return x * y;
+    return x * y;
 }
 function division(x, y) {
-  return x / y;
+    return x / y;
 }
 
-function log(x){
-  return Math.log10(x);
+function log(x) {
+    return Math.log10(x);
 }
 
-function power(x,y){
-  return Math.pow(x,y);
+function power(x, y) {
+    return Math.pow(x, y);
 }
 
-function factorial(x){
-  let power = 1;
-  for(let i=2;i<=x;i++){
-    power = power*i;
-  }
-  return power;
+function factorial(x) {
+    if (x === 0 || x === 1) {
+        return 1;
+    }
+
+    let power = 1;
+    for (let i = 2; i <= x; i++) {
+        power *= i;
+    }
+
+    return power;
 }
 
-
-
-export { sum, subtraction, multiplication, division, log, power, factorial  };
+export { sum, subtraction, multiplication, division, log, power, factorial };
